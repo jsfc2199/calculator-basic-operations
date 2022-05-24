d4379d604b093c73b894c3e8c269c8071140dc9a
Juan Sebastian <jsfc2199@gmail.com>
2022-05-23 22:15:54 -0500

    validating that if the first parameter of the division is 0, return 0

--- src/main/java/com/calculator/functional/programming/Application.java
+++ src/main/java/com/calculator/functional/programming/Application.java
@@ -14,11 +14,11 @@ public class Application {
 
         IMultiplication calculateMultiplication = (x, y) -> IntStream.iterate(x, i->i).limit(y).reduce((acum, num) -> calculateAddition.addition(acum, x)).getAsInt();
 
-        IDivision calculateDivision = (x, y) -> IntStream.rangeClosed(0, x).reduce((acum, num) -> calculateMultiplication.mult(acum, y) <= x ? acum+1 : acum).getAsInt()-1;
+        IDivision calculateDivision = (x, y) -> x==0?0:IntStream.rangeClosed(0, x).reduce((acum, num) -> calculateMultiplication.mult(acum, y) <= x ? acum+1 : acum).getAsInt()-1;
 
         System.out.println("The multiplication is: " + calculateMultiplication.mult(2, 6));
         System.out.println("The addition is: " + calculateAddition.addition(5, 8));
         System.out.println("The substraction is: " + calculateSubtraction.subtraction(8, 7));
-        System.out.println("The division is: " + calculateDivision.division(176, 16));
+        System.out.println("The division is: " + calculateDivision.division(0, 9));
     }
 }
