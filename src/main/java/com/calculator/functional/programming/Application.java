370ceeb7eee4a479b16747a3b55b5e073f07f8ef
Juan Sebastian <jsfc2199@gmail.com>
2022-05-23 22:10:37 -0500

    division added without validations

--- src/main/java/com/calculator/functional/programming/Application.java
+++ src/main/java/com/calculator/functional/programming/Application.java
@@ -3,6 +3,8 @@ package com.calculator.functional.programming;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
+import static java.lang.Math.abs;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -10,10 +12,13 @@ public class Application {
 
         ISubtraction calculateSubtraction = (x, y) -> x - y;
 
-        IMultiplication calculateMultiplication = (x,y)-> IntStream.rangeClosed(0,y).reduce((n,g)->calculateAddition.addition(n,x)).getAsInt();
+        IMultiplication calculateMultiplication = (x, y) -> IntStream.iterate(x, i->i).limit(y).reduce((acum, num) -> calculateAddition.addition(acum, x)).getAsInt();
 
-        System.out.println("The multiplication is: " + calculateMultiplication.mult(87,14));
-		System.out.println("The addition is: " + calculateAddition.addition(5,8));
-		System.out.println("The substraccion is: " + calculateSubtraction.subtraction(8,7));
+        IDivision calculateDivision = (x, y) -> IntStream.rangeClosed(0, x).reduce((acum, num) -> calculateMultiplication.mult(acum, y) <= x ? acum+1 : acum).getAsInt()-1;
+
+        System.out.println("The multiplication is: " + calculateMultiplication.mult(2, 6));
+        System.out.println("The addition is: " + calculateAddition.addition(5, 8));
+        System.out.println("The substraction is: " + calculateSubtraction.subtraction(8, 7));
+        System.out.println("The division is: " + calculateDivision.division(176, 16));
     }
 }
